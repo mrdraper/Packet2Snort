493444b7ccd478c0740792e69c6fd2380907896a
thesecuritystoic <thesecuritystoic@users.noreply.github.com>
2017-10-16 15:36:21 +0200

    First Commit
    First version of script + README

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # Packet2Snort
+
+This python script automatically generates a standard set of Snort IDS rules from a network packet out of a .pcap file.
+
+## Requirements
+
+- Python 2.7
+- scapy 
+- scapy-http
+- a valid .pcap file
+
+### Usage
+
+----\/---- Packet2Snort ----\/----
+
+This script parses a network packet from a PCAP file into a useable Snort rule for incident response, threat hunting and detection.
+
+Usage:
+packet2snort.py <options>
+
+Arguments: 
+
+-r <pcap> input pcap file
+-p <packetnr> input packet number in pcap
+-s to output snort rule from single packet
+
+### Output
+
+Currently, this script automatically outputs the following rules from a packet:
+- Basic IP/TCP(UDP)
+- ICMP From -> To + Type
+- HTTP Hostname
+- HTTP Filename
+- DNS Query
+- DNS Reply
+
+
+### Example
+
+1. Once we have analyzed a pcap file and determined that some packets are malicious and should be triggered upon by our Snort IDS, we can use this script to automatically generate rules from this known malicious packet.
+
+2. We need to know the packet number (easily spotted in a tool like WireShark). For this script, enter the packet number from WireShark -1, since WireShark starts counting at 1 and this script at 0. (Will try to fix later on). Alternatively, you can read the pcap with the -r switch and determine the packet number from there.
+
+3. Once we know the packet number, use the switch -p with the correct packet number (read point 2) to display the packet.
+
+4. Next, use the -s switch to generate snort rules from this packet. (Don't forget to change the sid to the number right for your environment.)
+
+5. Test, and then implement the rule(s) you want that have been generated. Win!
+
+### Known issues
+Some issues I encountered along the way:
+- Install scapy from source: https://github.com/secdev/scapy. When i installed from pip, my layers files were not up to date, thus i could not see DNS in TCP for example.
+
+### TO DO
+This script was just an experiment with Snort, Python and Scapy. The following are some things i want to add in the future:
+- HTTPS support
+	- SSL Certs, etc.
+- Support for multiple packet conversions (or entire pcaps)
+- SMB support
+- More advanced ruling on IP/TCP layer (Offset, checksum, etc.)
+- ...
+
+Feel free to submit pull requests or just leave some feedback. 
+
---
+++ packet2snort.py
@@ -0,0 +1,202 @@
+try:
+	from scapy.all import *
+except ImportError:
+	sys.stderr.write("ERROR: You must have scapy installed.\n")
+	sys.stderr.write("You can install it by running: sudo pip install -U 'scapy>=2.3,<2.4'")
+	exit(1)
+try:
+	from scapy.layers import http
+except ImportError:
+	sys.stderr.write("ERROR: You must have scapy-http installed.\n")
+	sys.stderr.write("You can install it by running: sudo pip install -U 'scapy>=1.8'")
+	exit(1)
+    
+import getopt
+import sys
+
+def usage():
+	print "----\/---- Packet2Snort ----\/----"
+	print "\nThis script parses a network packet from a PCAP file into a useable Snort rule for incident response, threat hunting and detection."
+	print "\nRequirements: \n- Scapy \n- Scapy-HTTP \n- Python 2.7"
+	print "\nUsage:\npacket2snort.py <options>\n"
+	print "Arguments: \n"
+	print "-r <pcap> input pcap file"
+	print "-p <packetnr> input packet number in pcap"
+	print "-s to output snort rule from single packet"
+	sys.exit(0)
+
+#converts layer 3 and 4 protocols into rules:
+# IP, TCP, UDP & ICMP
+def basicconvert(singlepacket):
+	try:
+		print "\n ----- Snort Rules ----- \n"
+# Print IP Layer Rules
+# Check if the IP layer is present in the packet
+		if IP in singlepacket:
+			print "------ Layer 3/4 Rules -------"
+			ipsource = singlepacket[IP].src
+			ipdest = singlepacket[IP].dst
+# Print TCP Layer Rules
+# Check if TCP is present in the packet
+			if TCP in singlepacket:
+				print "\n--- TCP ---\n"
+				tcpsourceport = singlepacket[TCP].sport
+				tcpdestport = singlepacket[TCP].dport
+				print ("alert tcp {0} {1}-> $HOME_NET any (msg: \"Suspicious IP {0} and port {1} detected!\"; reference:Packet2Snort; classtype:trojan-activity; sid:xxxx; rev:1;)".format(tcpsourceport, tcpdestport))
+				print ("alert tcp $HOME_NET any -> {0} {1} (msg: \"Suspicious IP {0} and port {1} detected!\"; reference:Packet2Snort; classtype:trojan-activity; sid:xxxx; rev:1;)".format(ipdest, tcpdestport))
+# Check if DNS is present in the packet				
+				if DNS in singlepacket:
+					print "\n--- DNS ---\n"
+					hostname = singlepacket[DNSQR].qname
+					if DNSRR in singlepacket:
+						hostaddr = singlepacket[DNSRR].rdata	
+						print ("alert udp any 53 -> $HOME_NET any (msg: \"Suspicious DNS reply for {0} with address {1} detected!\"; content:\"|00 01 00 01|\"; content:\"|00 04".format(hostname, hostaddr)),
+						addrsplit = hostaddr.split('.')
+						for addr in addrsplit:
+							hexaddr = format(int(addr), '02x')
+							print "\b",hexaddr.upper(),
+						print "\b|\"; distance:4; reference:Packet2Snort; classtype:trojan-activity; sid:xxxx; rev:1;)"
+					else:
+						print ("alert udp $HOME_NET any -> any 53 (msg: \"Suspicious DNS request for {0} detected!\"; content:\"|01 00 00 01 00 00 00 00 00 00|\"; depth:10; offset:2; content:\"".format(hostname)),
+						dnsplit = hostname.split('.')
+						for word in dnsplit:
+							if word != '':
+								numbers = len(word)
+								hexa = format(numbers, '02x')
+								upper = hexa.upper()
+								print ("\b|{0}|{1}".format(upper, word)),
+						print "\b\"; fast_pattern; nocase; distance:0; reference:Packet2Snort; classtype:trojan-activity; sid:xxxx; rev:1;)"				
+# Check if a HTTP request is present in the packet				
+				elif singlepacket.haslayer(http.HTTPRequest):
+					print "\n------ Layer 7 Rules ------"
+					print "\n--- HTTP ---\n"
+					httppacket = singlepacket.getlayer(http.HTTPRequest)
+					print ("HOST:\nalert tcp $HOME_NET any -> any $HTTP_PORTS (msg: \"Suspicious HTTP {0[Method]} request to {0[Host]} detected!\"; flow:established,to_server; content:\"Host|3a 20|{0[Host]}|0d 0a|\"; http_header; reference:Packet2Snort; classtype:trojan-activity; sid:xxxx; rev:1;)".format(httppacket.fields))
+					print ("\nFilename:\nalert tcp $HOME_NET any -> any $HTTP_PORTS (msg: \"Suspicious HTTP file name \"{0[Path]}\" requested at {0[Host]}!\"; flow:established,to_server; content:\"{0[Path]}\"; http_uri; reference:Packet2Snort; classtype:trojan-activity; sid:xxxx; rev:1;)".format(httppacket.fields))
+# Check if a HTTP response is present in the packet	(Currently not active)			
+#				elif singlepacket.haslayer(http.HTTPResponse):
+#					print "\n------ Layer 7 Rules ------"
+#					print "\n--- HTTP ---\n"
+#					httppacket2 = singlepacket.getlayer(http.HTTPResponse)
+#					print httppacket2
+# Print UDP Layer Rules
+# Check if UDP is present in the packet
+			elif UDP in singlepacket:
+				print "\n--- UDP ---\n"
+				udpsrcport = singlepacket[UDP].sport
+				udpdestport = singlepacket[UDP].dport
+				print ("alert udp {0} {1} -> any any (msg: \"Suspicious IP {0} and port {1} detected!\"; reference:Packet2Snort; classtype:trojan-activity; sid:xxxx; rev:1;)".format(ipsource, udpsrcport))
+				print ("alert udp any any -> {0} {1} (msg: \"Suspicious IP {0} and port {1} detected!\"; reference:Packet2Snort; classtype:trojan-activity; sid:xxxx; rev:1;)".format(ipdest, udpdestport))
+# Check if DNS is present in the packet				
+				if DNS in singlepacket:
+					print "\n--- DNS ---\n"
+					hostname = singlepacket[DNSQR].qname
+					if DNSRR in singlepacket:
+						hostaddr = singlepacket[DNSRR].rdata	
+						print ("alert udp any 53 -> $HOME_NET any (msg: \"Suspicious DNS reply for {0} with address {1} detected!\"; content:\"|00 01 00 01|\"; content:\"|00 04".format(hostname, hostaddr)),
+						addrsplit = hostaddr.split('.')
+						for addr in addrsplit:
+							hexaddr = format(int(addr), '02x')
+							print "\b",hexaddr.upper(),
+						print "\b|\"; distance:4; reference:Packet2Snort; classtype:trojan-activity; sid:xxxx; rev:1;)"
+					else:
+						print ("alert udp $HOME_NET any -> any 53 (msg: \"Suspicious DNS request for {0} detected!\"; content:\"|01 00 00 01 00 00 00 00 00 00|\"; depth:10; offset:2; content:\"".format(hostname)),
+						dnsplit = hostname.split('.')
+						for word in dnsplit:
+							if word != '':
+								numbers = len(word)
+								hexa = format(numbers, '02x')
+								upper = hexa.upper()
+								print ("\b|{0}|{1}".format(upper, word)),
+						print "\b|00|\"; fast_pattern; nocase; distance:0; reference:Packet2Snort; classtype:trojan-activity; sid:xxxx; rev:1;)"				
+# Print ICMP Layer Rules
+# Check if ICMP is present in the packet
+			elif ICMP in singlepacket:
+				print "--- ICMP ---\n"
+				icmptype = singlepacket[ICMP].type
+				print ("alert icmp {0} any -> {1} any (msg: \"Suspicious ICMP packet from {0} to {1} with type {2}!\"; icode:0; itype:{2}; reference:Packet2Snort; classtype:trojan-activity; sid:xxxx; rev:1;)".format(ipsource, ipdest, icmptype))
+# Throw error when no L4 protocols found
+			else:
+				print "No UDP/TCP Layer 4 Protocol Found!"
+				sys.exit(1)
+# Throw error when no IP found
+		else:
+			print "No IP Layer 3 Protocol Found!"
+			sys.exit(1)
+		print "\nDon't forget to change the sid of the generated rule(s)!"
+# Print error when they occur	
+	except Exception, e:
+		print "Error: ", e
+		print "\n"
+		usage()
+		pass
+
+#Let user input pcap
+def main():
+	try:
+#Let user input pcap
+		cap = None
+		packetnr = None
+		protocol = None
+		snortoutput = False
+		options, arguments = getopt.getopt(sys.argv[1:], "r:p:P:sh")
+
+#Check if argument is given and fill variables with arguments
+		if len(sys.argv) == 1:
+			usage()
+
+		for opt, args in options:
+			if opt in ('-r'):
+				cap = args
+			elif opt in ('-p'):
+				packetnr = args
+			elif opt in ('-h'):
+				usage()
+			elif opt in ('-s'):
+				snortoutput = True
+			else:
+				print "No arguments given"
+				sys.exit(1)
+
+# Check if pcap file exists
+		if cap:
+			if os.path.isfile(cap):
+				scapy_cap = rdpcap(cap)
+			else:
+				print "Error:", cap, "doest not exist."
+				sys.exit(1)
+		
+#Output summary of pcap
+		print "--------"
+		print "Summary: " + str(scapy_cap)
+		print "--------"
+# Check if a packet number has been supplied, and thus the variable packetnr is filled
+		if packetnr != None:
+			packetnr0 = int(packetnr) - 1
+			singlepacket = scapy_cap[int(packetnr0)]
+# Check if the -s paramater is give, and thus the script needs to output Snort.
+			if snortoutput == True:
+				basicconvert(singlepacket)
+# If the -s paramater is not give, just output the details of the packet selected.
+			else:
+				print str(scapy_cap[int(packetnr0)].show())
+# Check if the pcap file is given
+		elif cap != None:
+			countpacket = 1
+			for packet in scapy_cap:
+# Print a summary of each packet in the pcap, together with a packetnumber				
+				print str(countpacket), packet.summary()
+# Add a count to the packetnumber				
+				countpacket = countpacket + 1
+# When no argument is give, return the useage function		
+		else:
+			useage()
+# Print out the error, if received
+	except Exception, e:
+		print "Error: ", e
+		print "\n"
+		usage()
+		pass
+
+if __name__ == '__main__':
+	main()
